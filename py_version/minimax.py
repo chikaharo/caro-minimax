d7869ab13fa34375ba23839b84e37d34d7c4ae4d
chikaharo <madhattervn@gmail.com>
2024-11-19 15:56:23 +0700

    first commit

--- py_version/minimax.py
+++ py_version/minimax.py
@@ -5,15 +5,6 @@ import platform
 import time
 from os import system
 
-"""
-An implementation of Minimax AI Algorithm in Tic Tac Toe,
-using Python.
-This software is available under GPL license.
-Author: Clederson Cruz
-Year: 2017
-License: GNU GENERAL PUBLIC LICENSE (GPL)
-"""
-
 HUMAN = -1
 COMP = +1
 board = [
@@ -21,13 +12,18 @@ board = [
     [0, 0, 0],
     [0, 0, 0],
 ]
+# board = [
+#     [+1, -1, +1],
+#     [-1, -1, +1],
+#     [0, 0, 0],
+# ]
+
 
 
 def evaluate(state):
     """
-    Function to heuristic evaluation of state.
-    :param state: the state of the current board
-    :return: +1 if the computer wins; -1 if the human wins; 0 draw
+    Hàm đánh giá điểm.
+    :Trả về +1 nếu AI thắng, -1 nếu người chơi thắng, trả về 0 nếu hòa
     """
     if wins(state, COMP):
         score = +1
@@ -41,14 +37,10 @@ def evaluate(state):
 
 def wins(state, player):
     """
-    This function tests if a specific player wins. Possibilities:
-    * Three rows    [X X X] or [O O O]
-    * Three cols    [X X X] or [O O O]
-    * Two diagonals [X X X] or [O O O]
-    :param state: the state of the current board
-    :param player: a human or a computer
-    :return: True if the player wins
-    """
+    Hàm kiểm tra điều kiện thắng của người chơi hoặc AI
+    Nếu có 3 ô giống nhau trên cùng 1 hàng, cột hoặc đường chéo thì thắng
+    Người chơi hoặc AI win thì trả về True
+    """ 
     win_state = [
         [state[0][0], state[0][1], state[0][2]],
         [state[1][0], state[1][1], state[1][2]],
@@ -66,20 +58,10 @@ def wins(state, player):
 
 
 def game_over(state):
-    """
-    This function test if the human or computer wins
-    :param state: the state of the current board
-    :return: True if the human or computer wins
-    """
     return wins(state, HUMAN) or wins(state, COMP)
 
 
 def empty_cells(state):
-    """
-    Each empty cell will be added into cells' list
-    :param state: the state of the current board
-    :return: a list of empty cells
-    """
     cells = []
 
     for x, row in enumerate(state):
@@ -91,12 +73,6 @@ def empty_cells(state):
 
 
 def valid_move(x, y):
-    """
-    A move is valid if the chosen cell is empty
-    :param x: X coordinate
-    :param y: Y coordinate
-    :return: True if the board[x][y] is empty
-    """
     if [x, y] in empty_cells(board):
         return True
     else:
@@ -104,12 +80,6 @@ def valid_move(x, y):
 
 
 def set_move(x, y, player):
-    """
-    Set the move on board, if the coordinates are valid
-    :param x: X coordinate
-    :param y: Y coordinate
-    :param player: the current player
-    """
     if valid_move(x, y):
         board[x][y] = player
         return True
@@ -119,43 +89,32 @@ def set_move(x, y, player):
 
 def minimax(state, depth, player):
     """
-    AI function that choice the best move
-    :param state: current state of the board
-    :param depth: node index in the tree (0 <= depth <= 9),
-    but never nine in this case (see iaturn() function)
-    :param player: an human or a computer
-    :return: a list with [the best row, best col, best score]
+    Hàm minimax tìm đường đi tốt nhất
+    Trả về list gồm [the best row, best col, best score]
     """
     if player == COMP:
         best = [-1, -1, -infinity]
     else:
         best = [-1, -1, +infinity]
-
     if depth == 0 or game_over(state):
         score = evaluate(state)
         return [-1, -1, score]
-
     for cell in empty_cells(state):
         x, y = cell[0], cell[1]
         state[x][y] = player
         score = minimax(state, depth - 1, -player)
-        state[x][y] = 0
+        state[x][y] = 0     # backtracking
         score[0], score[1] = x, y
-
-        if player == COMP:
+        if player == COMP:      # Trường hợp người chơi là AI
             if score[2] > best[2]:
-                best = score  # max value
-        else:
+                best = score  # Trả về max value 
+        else:       # Trường hợp người chơi là human
             if score[2] < best[2]:
-                best = score  # min value
-
+                best = score  # Trả về min value
     return best
 
 
 def clean():
-    """
-    Clears the console
-    """
     os_name = platform.system().lower()
     if 'windows' in os_name:
         system('cls')
@@ -164,11 +123,6 @@ def clean():
 
 
 def render(state, c_choice, h_choice):
-    """
-    Print the board on console
-    :param state: current state of the board
-    """
-
     chars = {
         -1: h_choice,
         +1: c_choice,
@@ -185,13 +139,6 @@ def render(state, c_choice, h_choice):
 
 
 def ai_turn(c_choice, h_choice):
-    """
-    It calls the minimax function if the depth < 9,
-    else it choices a random coordinate.
-    :param c_choice: computer's choice X or O
-    :param h_choice: human's choice X or O
-    :return:
-    """
     depth = len(empty_cells(board))
     if depth == 0 or game_over(board):
         return
@@ -212,12 +159,6 @@ def ai_turn(c_choice, h_choice):
 
 
 def human_turn(c_choice, h_choice):
-    """
-    The Human plays choosing a valid move.
-    :param c_choice: computer's choice X or O
-    :param h_choice: human's choice X or O
-    :return:
-    """
     depth = len(empty_cells(board))
     if depth == 0 or game_over(board):
         return
@@ -251,9 +192,6 @@ def human_turn(c_choice, h_choice):
 
 
 def main():
-    """
-    Main function that calls all functions
-    """
     clean()
     h_choice = ''  # X or O
     c_choice = ''  # X or O
